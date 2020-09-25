23554a25b2962f4d9a60539afc16b9331d9207c8
Chris Weir <chris.weir@qanw.co.uk>
2020-09-25 15:59:26 +0100

    end of section 3 video 3. Missed logic for social icons. Didnt want to copy the svg code

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class GamesController extends Controller
 {
@@ -66,7 +67,7 @@ class GamesController extends Controller
             ->json();
         abort_if($game[0]['status'] ?? 0 === 400, 404);
         return view('show', [
-            'game' => $game[0]
+            'game' => $this->formatGameForView($game[0])
         ]);
     }
 
@@ -102,5 +103,45 @@ class GamesController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    private function formatGameForView($game)
+    {
+        $temp = collect($game)->merge(
+            [
+                'coverImageUrl' => Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']),
+                'genres' => collect($game['genres'])->pluck('name')->implode(', '),
+                'involvedCompanies' => $game['involved_companies'][0]['company']['name'] ?? '',
+                'platforms' => collect($game['platforms'])->pluck('abbreviation')->implode(', '),
+                'memberRating' => array_key_exists('rating', $game) ? round($game['rating']) . '%' : '0%',
+                'criticRating' => array_key_exists('aggregated_rating', $game) ? round(
+                        $game['aggregated_rating']
+                    ) . '%' : '0%',
+                'trailer' => 'https://youtube.com/watch/' . $game['videos'][0]['video_id'],
+                'screenshots' => collect($game['screenshots'])->map(
+                    function ($screenshot) {
+                        return [
+                            'huge' => Str::replaceFirst('thumb', 'screenshot_huge', $screenshot['url']),
+                            'big' => Str::replaceFirst('thumb', 'screenshot_big', $screenshot['url'])
+                        ];
+                    }
+                )->take(9),
+                'similarGames' => collect($game['similar_games'])->map(function ($game) {
+                    return collect($game)->merge(
+                        [
+                            'coverImageUrl' => array_key_exists('cover', $game)
+                            ? Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) :
+                                'https://via.placeholder.com/264x352',
+                            'rating' => isset($game['rating']) ? round($game['rating']).'%' : null,
+                            'platforms' => array_key_exists('platforms', $game)
+                            ? collect($game['platforms'])->pluck('abbreviation')->implode(', ')
+                                : null,
+                        ]
+                    );
+                })->take(6)
+            ]
+        );
+
+        return $temp;
     }
 }
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -4,40 +4,28 @@
     <div class="container mx-auto px-4">
         <div class="flex game-details border-b border-gray-800 pb-12 flex flex-col lg:flex-row">
             <div class="flex-none">
-                <img src="{{Str::replaceFirst('thumb', 'cover_big', $game['cover']['url'])}}" alt="cover">
+                <img src="{{ $game['coverImageUrl'] }}" alt="cover">
             </div>
             <div class="lg:ml-12 lg:mr-64">
                 <h2 class="font-semibold text-4xl leading-tight mt-2">{{$game['name']}}</h2>
                 <div class="text-gray-400">
                     <span>
-                        @foreach($game['genres'] as $genre)
-                            {{$genre['name']}},
-                        @endforeach
+                        {{ $game['genres'] }}
                     </span>
                     &middot;
-                    @if(array_key_exists('involved_companies', $game))
                     <span>
-                        {{$game['involved_companies'][0]['company']['name']}}
+                        {{ $game['involvedCompanies'] }}
                     </span>
                     &middot;
-                    @endif
                     <span>
-                        @foreach($game['platforms'] as $platform)
-                            @if(array_key_exists('abbreviation', $platform))
-                                {{ $platform['abbreviation']}},
-                            @endif
-                        @endforeach
+                        {{ $game['platforms'] }}
                     </span>
                 </div>
                 <div class="flex flex-wrap items-center mt-8">
                     <div class="flex items-center">
                         <div class="w-16 h-16 bg-gray-800 rounded-full">
                             <div class="font-semibold text-xs flex justify-center items-center h-full">
-                                @if(array_key_exists('rating', $game))
-                                    {{ round($game['rating']).'%' }}
-                                @else
-                                    0%
-                                @endif
+                                {{ $game['memberRating'] }}
                             </div>
                         </div>
                         <div class="ml-4 text-xs">Member <br/> Score</div>
@@ -45,11 +33,7 @@
                     <div class="flex items-center ml-12">
                         <div class="w-16 h-16 bg-gray-800 rounded-full">
                             <div class="font-semibold text-xs flex justify-center items-center h-full">
-                                @if(array_key_exists('aggregated_rating', $game))
-                                    {{ round($game['aggregated_rating']).'%' }}
-                                @else
-                                    0%
-                                @endif
+                                {{ $game['criticRating'] }}
                             </div>
                         </div>
                         <div class="ml-4 text-xs">Critic <br/> Score</div>
@@ -60,9 +44,9 @@
                 </p>
 
                 <div class="mt-12">
-                    <button class="flex bg-blue-500 text-white font-semibold px-4 py-4 hover:bg-blue-600 rounded transition ease-in-out duration-150">
+                    <a href="{{ $game['trailer'] }}" class="inline-flex bg-blue-500 text-white font-semibold px-4 py-4 hover:bg-blue-600 rounded transition ease-in-out duration-150">
                         <span class="ml-2">Play Trailer</span>
-                    </button>
+                    </a>
                 </div>
             </div>
         </div> <!-- end game details -->
@@ -73,8 +57,8 @@
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 mt-8">
                 @foreach($game['screenshots'] as $screenshot)
                     <div>
-                        <a href="{{Str::replaceFirst('thumb', 'screenshot_huge', $screenshot['url'])}}">
-                            <img src="{{Str::replaceFirst('thumb', 'screenshot_big', $screenshot['url'])}}" alt=""
+                        <a href="{{ $screenshot['huge'] }}">
+                            <img src="{{ $screenshot['big'] }}" alt=""
                                  class="hover:opacity-75 transition ease-in-out duration-150">
                         </a>
                     </div>
@@ -86,22 +70,20 @@
                 Similar Games
             </h2>
             <div class="similar-games text-sm grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 xl:grid-cols-6 gap-12">
-                @foreach($game['similar_games'] as $game)
+                @foreach($game['similarGames'] as $game)
                     <div class="game mt-8">
                         <div class="relative inline-block">
-                            @if(array_key_exists('cover', $game))
-                                <a href="{{route('games.show', $game['slug'])}}">
-                                    <img src="{{Str::replaceFirst('thumb', 'cover_big', $game['cover']['url'])}}"
-                                         alt="game cover"
-                                         class="hover:opacity-75 transition ease-in-out duration-150">
-                                </a>
-                            @endif
-                            @if (isset($game['rating']))
+                            <a href="{{route('games.show', $game['slug'])}}">
+                                <img src="{{ $game['coverImageUrl'] }}"
+                                     alt="game cover"
+                                     class="hover:opacity-75 transition ease-in-out duration-150">
+                            </a>
+                            @if ($game['rating'])
                                 <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-800 rounded-full"
                                      style="right:-20px; bottom: -20px"
                                 >
                                     <div class="font-semibold text-xs flex justify-center items-center h-full">
-                                        {{round($game['rating']).'%'}}
+                                        {{ $game['rating'] }}
                                     </div>
                                 </div>
                             @endif
@@ -110,13 +92,7 @@
                             {{$game['name']}}
                         </a>
                         <div class="text-gray-400 mt-1">
-                            @if(array_key_exists('platforms', $game))
-                                @foreach($game['platforms'] as $platform)
-                                    @if(array_key_exists('abbreviation', $platform))
-                                        {{ $platform['abbreviation']}},
-                                    @endif
-                                @endforeach
-                            @endif
+                            {{ $game['platforms'] }}
                         </div>
                     </div>
                 @endforeach()
