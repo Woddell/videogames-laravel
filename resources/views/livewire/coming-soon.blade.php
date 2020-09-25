6ba5426d3d975880eed60c170ead1fc0f147f73a
Chris Weir <chris.weir@qanw.co.uk>
2020-09-25 16:22:45 +0100

    end of section 4

--- resources/views/livewire/coming-soon.blade.php
+++ resources/views/livewire/coming-soon.blade.php
@@ -1,30 +1,9 @@
 <div wire:init="loadComingSoon" class="most-anticipated-container space-y-10 mt-8">
     @forelse($comingSoon as $game)
-        <div class="game flex">
-            <a href="#">
-                <img src="{{ $game['coverImageUrl'] }}" alt="doom"
-                     class="w-16 hover:opacity-75 transition ease-in-out duration-150">
-            </a>
-            <div class="ml-8">
-                <a href="#" class="hover:text-gray-300">{{$game['name']}}</a>
-                <div class="text-gray-400 text-sm mt-1">
-                    {{ $game['releaseDate'] }}
-                </div>
-            </div>
-        </div>
+        <x-game-card-small :game="$game" />
     @empty
         @foreach(range(1, 4) as $game)
-            <div class="most-anticipated-container space-y-10 mt-8">
-                <div class="game flex">
-                    <div class="bg-gray-800 w-16 h-20 flex-none"></div>
-                    <div class="ml-4">
-                        <div class="text-transparent bg-gray-700 rounded leading-tight">title goes here today</div>
-                        <div class="mt-2 inline-block text-sm text-transparent bg-gray-700 rounded leading-tight">
-                            Jan 01, 2020
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <x-game-card-small-skeleton />
         @endforeach
     @endforelse
 </div>
--- resources/views/livewire/most-anticipated.blade.php
+++ resources/views/livewire/most-anticipated.blade.php
@@ -1,33 +1,9 @@
 <div wire:init="loadMostAnticipated" class="most-anticipated">
     @forelse($mostAnticipated as $game)
-        <div class="most-anticipated-container space-y-10 mt-8">
-            <div class="game flex">
-                <a href="#">
-                    <img src="{{ $game['coverImageUrl'] }}"
-                         alt="game cover"
-                         class="w-16 hover:opacity-75 transition ease-in-out duration-150">
-                </a>
-                <div class="ml-4">
-                    <a href="#" class="hover:text-gray-300">{{$game['name']}}</a>
-                    <div class="text-gray-400 text-sm mt-1">
-                        {{ $game['releaseDate'] }}
-                    </div>
-                </div>
-            </div>
-        </div>
+        <x-game-card-small :game="$game" />
     @empty
-    @foreach(range(1, 4) as $game)
-        <div class="most-anticipated-container space-y-10 mt-8">
-            <div class="game flex">
-                <div class="bg-gray-800 w-16 h-20 flex-none"></div>
-                <div class="ml-4">
-                    <div class="text-transparent bg-gray-700 rounded leading-tight">title goes here today</div>
-                    <div class="mt-2 inline-block text-sm text-transparent bg-gray-700 rounded leading-tight">
-                        Jan 01, 2020
-                    </div>
-                </div>
-            </div>
-        </div>
-    @endforeach
+        @foreach(range(1, 4) as $game)
+            <x-game-card-small-skeleton />
+        @endforeach
     @endforelse
 </div>
