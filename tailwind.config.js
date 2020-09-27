09b1304efe1de714546dca2a52dcdf2c516ce045
Chris Weir <chris.weir@qanw.co.uk>
2020-09-27 21:43:29 +0100

    finished application

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,15 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: [],
+  purge: [
+    './app/**/*.php',
+    './resources/**/*.php',
+  ],
   theme: {
     extend: {
       spacing: {
-        '44': '11rem'
-      }
+        '44': '11rem',
+      },
     },
     spinner: (theme) => ({
       default: {
@@ -16,11 +19,11 @@ module.exports = {
         size: '1em',
         border: '2px',
         speed: '500ms',
-      }
-    })
+      },
+    }),
   },
   variants: {},
   plugins: [
-    require('tailwindcss-spinner')
+    require('tailwindcss-spinner'),
   ],
 }
